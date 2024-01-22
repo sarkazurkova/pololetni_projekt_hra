7f89236026bdca8deae041ca1d19dfe20e7876ee
sarka137 <120708285+sarka137@users.noreply.github.com>
2024-01-22 19:45:39 +0100

    3 - grafika, body

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 let cat;
+let score = document.getElementById("skore");
 let can = [];
 let frame = 0;
-let points = 0
+let points = 0;
+let lives = 10;
+let speed = 60;
+let catImage, canImage;
+
 
 class Cat {
     constructor(x=0, y=0) {
@@ -24,22 +29,23 @@ class Cat {
 
     draw(){
         this.move();
-        //push();    
+        push();    
         rect(this.x, this.y, this.w, this.h);
-        fill(this.color);
+        //fill(this.color);
         rectMode(CENTER);
-        //pop();
+        image(catImage, this.x -30, this.y - 30, 100, 100 );
+        pop();
     }
 }
 
 class Can {
-    constructor(x, y, angle) {
+    constructor(angle) {
         this.y = -100;
         this.size = random(20, 40);
-        this.x = random(0, width - this.size);
+        this.x = random(10, width - this.size);
         this.angle = angle;
         
-        this.v = 3;
+        this.v = 2;
         this.angle = random(0, 359);
     }
 
@@ -47,36 +53,53 @@ class Can {
         this.y += this.v;
     }
 
+    
+
     draw() {
         this.move();
+        
         push();
-        stroke(255);
-        fill('red');
-        circle(this.x, this.y, this.size);
+        //stroke(255);
+        //dfill('red');
+        
+        imageMode(CENTER);
+        
+        
+        circle(this.x+this.size/2, this.y+this.size/2, this.size);
+        
+       image(canImage, this.x+ this.size /2 , this.y + this.size /2, this.size*3, this.size*3 );
+       
         pop();
     }
 }
 
+function preload() {
+    catImage = loadImage("img/cat2.png");
+    canImage = loadImage("img/can.png");
+}
 
 function setup() {
     canvas = createCanvas(500, 500);
-    cat = new Cat(0, 490);
+    cat = new Cat(0, height-50);
     
 }
 
 function draw() {
     frame++;
-    background(0);
+    
+    background(209,237,242);
     cat.draw();
-    if(frame % 60 == 0){
+    if(frame % speed == 0){
         can.push(new Can());
-    }
+        }
+    
     
     can.forEach(function(c, idx, arr){
 
         c.draw();
         if(c.y > height ) {
             arr.splice(idx, 1);
+            lives --;
         }
         if(collideRectCircle(
             cat.x,
@@ -92,10 +115,30 @@ function draw() {
         }
         
     });
-    console.log(points);
-    
-}
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
+
+
+
+    console.log(points);
+    console.log(lives);
+    console.log(speed);
+    
+    
+    if(lives == 0) {
+        background(0);
+        noLoop();
+         /* Nastavení tmavě červeného pozadí s průhledností (200) */
+            background(100, 0, 0, 200);
+            textSize(50);
+            fill(255, 0, 0, 200);
+            text('GAME OVER', width/2 - 200, height/2);
+        
+        
+        
+    }
+    score.innerHTML = `<p>Skóre: ${points}</p>`;
+    score.innerHTML += `<p>Životy: ${lives}</p>`;
+} 
+
+
+
